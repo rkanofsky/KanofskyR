72ea034f6ac40790abf670d406d7933a853f8898
rkanofsky <91801101+rkanofsky@users.noreply.github.com>
2024-04-21 12:08:17 -0400

    renamed Main to Sample

--- src/main/java/org/example/Sample.java
+++ src/main/java/org/example/Sample.java
@@ -2,7 +2,7 @@ package org.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Sample {
     public int sum(int a, int b) {
         return a + b;
     }
@@ -12,7 +12,7 @@ public class Main {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        Main main = new Main();
+        Sample main = new Sample();
         main.sum(10, 10);
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
