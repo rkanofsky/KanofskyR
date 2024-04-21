bf5340f8c358e08c518ec394a1dff419e9d827a0
rkanofsky <91801101+rkanofsky@users.noreply.github.com>
2024-04-21 11:59:11 -0400

    testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,8 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         Main main = new Main();
--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -1,3 +1,5 @@
+package org.example;
+
 public class Sum {
     public int sum(int a, int b) {
         return a + b;
